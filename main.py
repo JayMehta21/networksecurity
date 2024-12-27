a7d6f1a0094a1793a450d384af15aec3b2896af0
JayMehta21 <jaykalpeshmehta@gmail.com>
2024-12-27 13:25:42 +0530

    Data Validation Implementation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
 import sys
 
 if __name__ == "__main__":
@@ -25,7 +26,23 @@ if __name__ == "__main__":
         logging.info(f"Data Ingestion Completed. Artifact: {data_ingestion_artifact}")
         print(data_ingestion_artifact)
 
+        # Instantiate the data validation configuration
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+
+        # Create data validation object
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiating the data validation process.")
+
+        # Execute the data validation process
+        data_validation_artifact = data_validation.initiate_data_validation()
+
+        # Log and print the data validation artifact
+        logging.info(f"Data Validation Completed. Artifact: {data_validation_artifact}")
+        print(data_validation_artifact)
     except Exception as e:
         # Handle exceptions by wrapping them in NetworkSecurityException
         logging.error(f"An error occurred: {e}")
         raise NetworkSecurityException(e, sys)
+
+
+
--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,23 +1,32 @@
-import sys 
-from networksecurity.logging import logger 
+import sys
+from networksecurity.logging import logger
+
 class NetworkSecurityException(Exception):
-    def __init__(self,error_message ,error_details:sys):
+    def __init__(self, error_message, error_details: sys):
         self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
-
-
-        self.lineno = exc_tb.tb_lineno
-        self.filename = exc_tb.tb_frame.f_code.co_filename
+        # If error_details is None or doesn't have traceback, we handle it gracefully
+        if error_details and hasattr(error_details, 'exc_info'):
+            _, _, exc_tb = error_details.exc_info()
+            if exc_tb:
+                self.lineno = exc_tb.tb_lineno
+                self.filename = exc_tb.tb_frame.f_code.co_filename
+            else:
+                # If no traceback, use default info
+                self.lineno = 'N/A'
+                self.filename = 'N/A'
+        else:
+            self.lineno = 'N/A'
+            self.filename = 'N/A'
 
     def __str__(self):
-        return "error occured in python script [{0}] line number [{1}] error message ".format()
-        self.filename , self.lineno , str(self.error_message)
+        return "error occurred in python script [{0}] line number [{1}] error message [{2}]".format(
+            self.filename, self.lineno, str(self.error_message))
 
-    
+
 if __name__ == "__main__":
-    try: 
+    try:
         logger.logging.info("enter the try block ")
-        a = 1/0 
+        a = 1 / 0  # This will raise a ZeroDivisionError
         print("this will not be printed", a)
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e, sys)
