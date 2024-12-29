659e29a695a39dd414e53de16a2df5fb5ef16590
JayMehta21 <jaykalpeshmehta@gmail.com>
2024-12-29 11:51:27 +0530

    Training Evaluation and mlflow integration

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -16,6 +16,7 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier,
 )
+import mlflow
 
 class ModelTrainer:
     def __init__(self, model_trainer_config: ModelTrainerConfig, data_transformation_artifact: DataTransformationArtifact):
@@ -24,6 +25,17 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+        
+    def track_mlflow(self,best_model , classificationmetric):
+        with mlflow.start_run():
+            f1_score = classificationmetric.f1_score
+            precision_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision_score",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
 
     def train_model(self, x_train, y_train, x_test, y_test):
         try:
@@ -71,9 +83,16 @@ class ModelTrainer:
             y_train_pred = best_model.predict(x_train)
             classification_train_metric = get_classification_score(y_true=y_train, y_pred=y_train_pred)
 
+
+            ##track the experiments with mlflow 
+            self.track_mlflow(best_model,classification_train_metric)
+
+
             ## Testing metrics
             y_test_pred = best_model.predict(x_test)
             classification_test_metric = get_classification_score(y_true=y_test, y_pred=y_test_pred)
+
+            ##self.track_mlflow(best_model,classification_train_metric)
 
             ## Preprocessor loading
             preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
